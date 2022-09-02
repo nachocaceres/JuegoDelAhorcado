b406717a217f0530baac6693f103d4332471d125
nachocaceres <nachocaceres99@gmail.com>
2022-09-02 20:14:33 -0300

    Primer Commit

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const botonAgregarPalabra = document.getElementById("agregar");
+const botonesGeneral = document.getElementById("botones-general");
+
+botonAgregarPalabra.onclick = () =>{
+    botonesGeneral.classList.add("ocultar");
+    
+}
+
+
+
+// function ocultarContenedor(){
+//     botonesGeneral.classList.add("ocultar")
+// }
